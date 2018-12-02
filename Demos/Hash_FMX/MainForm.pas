c5bb927cb71f24eb5e6b558a4e5602aac5637d0a
MHumm <Markus.Humm@googlemail.com>
2018-12-02 17:07:54 +0100

    Release version of DEC hash demo FMX built and published via Google Play store. It still only builds if debug DCU use is turned on and debug and reference information is included.

--- Demos/Hash_FMX/MainForm.pas
+++ Demos/Hash_FMX/MainForm.pas
@@ -1,3 +1,64 @@
+{*****************************************************************************
+
+  Delphi Encryption Compendium (DEC)
+  Version 6.0
+
+  Copyright (c) 2016 - 2018 Markus Humm (markus [dot] humm [at] googlemail [dot] com)
+  Copyright (c) 2008 - 2012 Frederik A. Winkelsdorf (winkelsdorf [at] gmail [dot] com)
+  Copyright (c) 1999 - 2008 Hagen Reddmann (HaReddmann [at] T-Online [dot] de)
+  All rights reserved.
+
+                               *** License ***
+
+  This file is part of the Delphi Encryption Compendium (DEC). The DEC is free
+  software being offered under a dual licensing scheme: BSD or MPL 1.1.
+
+  The contents of this file are subject to the Mozilla Public License (MPL)
+  Version 1.1 (the "License"); you may not use this file except in compliance
+  with the License. You may obtain a copy of the License at
+  http://www.mozilla.org/MPL/
+
+  Alternatively, you may redistribute it and/or modify it under the terms of
+  the following Berkeley Software Distribution (BSD) license:
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+  THE POSSIBILITY OF SUCH DAMAGE.
+
+                        *** Export/Import Controls ***
+
+  This is cryptographic software. Even if it is created, maintained and
+  distributed from liberal countries in Europe (where it is legal to do this),
+  it falls under certain export/import and/or use restrictions in some other
+  parts of the world.
+
+  PLEASE REMEMBER THAT EXPORT/IMPORT AND/OR USE OF STRONG CRYPTOGRAPHY
+  SOFTWARE OR EVEN JUST COMMUNICATING TECHNICAL DETAILS ABOUT CRYPTOGRAPHY
+  SOFTWARE IS ILLEGAL IN SOME PARTS OF THE WORLD. SO, WHEN YOU IMPORT THIS
+  PACKAGE TO YOUR COUNTRY, RE-DISTRIBUTE IT FROM THERE OR EVEN JUST EMAIL
+  TECHNICAL SUGGESTIONS OR EVEN SOURCE PATCHES TO THE AUTHOR OR OTHER PEOPLE
+  YOU ARE STRONGLY ADVISED TO PAY CLOSE ATTENTION TO ANY EXPORT/IMPORT AND/OR
+  USE LAWS WHICH APPLY TO YOU. THE AUTHORS OF THE DEC ARE NOT LIABLE FOR ANY
+  VIOLATIONS YOU MAKE HERE. SO BE CAREFUL, IT IS YOUR RESPONSIBILITY.
+
+*****************************************************************************}
 unit MainForm;
 
 interface
@@ -34,8 +95,6 @@ type
     procedure EditInputChangeTracking(Sender: TObject);
     procedure EditInputKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
       Shift: TShiftState);
-    procedure FormKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
-      Shift: TShiftState);
   private
     /// <summary>
     ///   Lists all available hash classes in the hash classes combo box
@@ -46,16 +105,13 @@ type
     ///   combo boxes
     /// </summary>
     procedure InitFormatCombos;
-    procedure ShowErrorMessage(ErrorMsg: string);
-    {$IFDEF ANDROID}
     /// <summary>
-    ///   Calls the Android home screen into foreground so that when pressing
-    ///   back the app goes into background properly and when pressing the app
-    ///   icon in the app drawer or on home screen it returns to the same point
-    ///   it was.
+    ///   Displays a given error message text in a non modal message box
     /// </summary>
-    procedure OpenHomeScreen;
-    {$ENDIF}
+    /// <param name="ErrorMsg">
+    ///   Text to show as error message
+    /// </param>
+    procedure ShowErrorMessage(ErrorMsg: string);
   public
   end;
 
@@ -178,17 +234,6 @@ begin
   InitFormatCombos;
 end;
 
-procedure TMainForm.FormKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
-  Shift: TShiftState);
-begin
-  {$IFDEF Android}
-  if Key = vkHardwareBack then
-    OpenHomeScreen;
-
-  Key := 0;
-  {$ENDIF}
-end;
-
 procedure TMainForm.InitFormatCombos;
 var
   MyClass : TPair<Int64, TDECClass>;
@@ -249,24 +294,5 @@ begin
   LayoutTop.Width    := VertScrollBox1.Width;
   LayoutBottom.Width := VertScrollBox1.Width;
 end;
-
-{$IFDEF ANDROID}
-procedure TMainForm.OpenHomeScreen;
-{$IFDEF ANDROID}
-var
-  Intent: JIntent;
-{$ENDIF ANDROID}
-begin
-{$IFDEF ANDROID}
-  Intent := TJIntent.Javaclass.init(TJIntent.JavaClass.ACTION_MAIN);
-  Intent.addCategory(TJIntent.JavaClass.CATEGORY_HOME);
-  Intent.setFlags(TjIntent.JavaClass.FLAG_ACTIVITY_NEW_TASK);
-  TAndroidhelper.Activity.startActivity(Intent);
-{$ENDIF ANDROID}
-{$IFDEF IOS}
-  NavigationController.popToRootViewControllerAnimated(true);
-{$ENDIF}
-end;
-{$ENDIF}
 
 end.
