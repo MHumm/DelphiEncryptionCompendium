688ca56369cbb79964c78f3a6added1d432f69bd
Markus Humm <markus.humm@gmail.com>
2020-12-19 17:17:26 +0100

    Added missing header comments

--- Demos/Progress_VCL/MainForm.pas
+++ Demos/Progress_VCL/MainForm.pas
@@ -1,3 +1,24 @@
+{*****************************************************************************
+  The DEC team (see file NOTICE.txt) licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License. A copy of this licence is found in the root directory of
+  this project in the file LICENCE.txt or alternatively at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*****************************************************************************}
+
+/// <summary>
+///   Simple demonstration of using the IDECProgress interface for displaying
+///   progress of an operation
+/// </summary>
 unit MainForm;
 
 interface
