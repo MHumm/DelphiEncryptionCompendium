a9d6792d09b6a6301391a0dc43699a9468e1ee26
Markus Humm <markus.humm@googlemail.com>
2021-07-08 17:05:14 +0200

    Taking over some fixes from 6.2.1 fix branch for compiling and improved unit tests for class registration mechanism

--- Demos/HashBenchmark_FMX/MainForm.pas
+++ Demos/HashBenchmark_FMX/MainForm.pas
@@ -183,6 +183,11 @@ end;
 procedure TFormMain.FormCreate(Sender: TObject);
 begin
   b_CopyToClipboard.Enabled := false;
+
+  // This property is only supported from 10.4 onwards, so we set it in code
+  {$IF CompilerVersion >= 34.0}
+  StringColumn2.HorzAlign := TTextAlign.Trailing;
+  {$ENDIF}
 end;
 
 procedure TFormMain.FormResize(Sender: TObject);
