9612b22af96a974089016890e45411c3aa0b0230
SchneiderInfosystems <webmaster@schneider-infosys.ch>
2024-12-19 19:26:46 +0100

    Extend this demo to offer encryption/decryption Allow entering key and IV or allow to create a pair in base64 notation Used for testing PKCS7 in streams

--- Demos/Progress_VCL/MainFormProgressVLC.dfm
+++ Demos/Progress_VCL/MainFormProgressVLC.dfm
@@ -1,8 +1,8 @@
 object FormMain: TFormMain
   Left = 0
   Top = 0
-  Caption = 'ProgressTest'
-  ClientHeight = 187
+  Caption = 'Progress Test for File Encrypt/Decrypt'
+  ClientHeight = 319
   ClientWidth = 635
   Color = clBtnFace
   Constraints.MinHeight = 226
@@ -12,60 +12,142 @@ object FormMain: TFormMain
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   DesignSize = (
     635
-    187)
+    319)
   TextHeight = 13
-  object Button1: TButton
-    Left = 8
-    Top = 16
-    Width = 75
-    Height = 25
-    Caption = 'Encrypt'
-    TabOrder = 0
-    OnClick = Button1Click
-  end
-  object Edit1: TEdit
-    Left = 104
-    Top = 18
-    Width = 523
-    Height = 21
-    Anchors = [akLeft, akTop, akRight]
-    TabOrder = 1
-    Text = 'D:\Test.txt'
-  end
   object ProgressBar1: TProgressBar
     Left = 8
-    Top = 64
+    Top = 193
     Width = 619
     Height = 17
     Anchors = [akLeft, akTop, akRight]
-    TabOrder = 2
+    TabOrder = 0
   end
   object RadioButtonMethod: TRadioButton
     Left = 8
-    Top = 96
+    Top = 216
     Width = 193
     Height = 17
     Caption = 'Use method as progress event'
     Checked = True
-    TabOrder = 3
+    TabOrder = 1
     TabStop = True
   end
   object RadioButtonProcedure: TRadioButton
     Left = 8
-    Top = 128
+    Top = 248
     Width = 193
     Height = 17
     Caption = 'Use procedure as progress event'
-    TabOrder = 4
+    TabOrder = 2
   end
   object RadioButtonAnonMethod: TRadioButton
     Left = 8
-    Top = 162
+    Top = 282
     Width = 233
     Height = 17
     Caption = 'Use anonymous method as progress event'
+    TabOrder = 3
+  end
+  object CheckBoxPKCS7: TCheckBox
+    Left = 8
+    Top = 98
+    Width = 121
+    Height = 17
+    Anchors = [akTop, akRight]
+    Caption = 'Use PKCS7'
+    TabOrder = 4
+  end
+  object PageControl1: TPageControl
+    Left = 8
+    Top = 130
+    Width = 619
+    Height = 57
+    ActivePage = tabEncrypt
     TabOrder = 5
+    object tabEncrypt: TTabSheet
+      Caption = 'Encrypt AES256-CBC'
+      DesignSize = (
+        611
+        29)
+      object ButtonEncrypt: TButton
+        Left = 7
+        Top = 3
+        Width = 75
+        Height = 25
+        Caption = 'Encrypt'
+        TabOrder = 0
+        OnClick = ButtonEncryptClick
+      end
+      object EditEncrypt: TEdit
+        Left = 88
+        Top = 5
+        Width = 523
+        Height = 21
+        Anchors = [akLeft, akTop, akRight]
+        TabOrder = 1
+        Text = 'D:\Test.txt'
+      end
+    end
+    object tabDecrypt: TTabSheet
+      Caption = 'Decrypt AES256-CBC'
+      ImageIndex = 1
+      DesignSize = (
+        611
+        29)
+      object ButtonDecrypt: TButton
+        Left = 7
+        Top = 3
+        Width = 75
+        Height = 25
+        Caption = 'Decrypt'
+        TabOrder = 0
+        OnClick = ButtonDecryptClick
+      end
+      object EditDecrypt: TEdit
+        Left = 88
+        Top = 5
+        Width = 523
+        Height = 21
+        Anchors = [akLeft, akTop, akRight]
+        TabOrder = 1
+        Text = 'D:\Test.enc'
+      end
+    end
+  end
+  object EditKey: TLabeledEdit
+    Left = 8
+    Top = 24
+    Width = 529
+    Height = 21
+    EditLabel.Width = 99
+    EditLabel.Height = 13
+    EditLabel.Caption = 'Key (base64 format)'
+    TabOrder = 6
+    Text = ''
+  end
+  object EditIV: TLabeledEdit
+    Left = 8
+    Top = 63
+    Width = 529
+    Height = 21
+    EditLabel.Width = 141
+    EditLabel.Height = 13
+    EditLabel.Caption = 'Initial Vector (base64 format)'
+    TabOrder = 7
+    Text = ''
+  end
+  object ButtonCreateKeyAndIV: TButton
+    Left = 552
+    Top = 24
+    Width = 75
+    Height = 64
+    Caption = 'Create Key and IV'
+    TabOrder = 8
+    WordWrap = True
+    OnClick = ButtonCreateKeyAndIVClick
   end
 end
--- Demos/Progress_VCL/MainFormProgressVLC.pas
+++ Demos/Progress_VCL/MainFormProgressVLC.pas
@@ -25,18 +25,33 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls, DECTypes;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.Mask, Vcl.ExtCtrls,
+  DECTypes, DECCipherBase, DECCiphers, DECFormat, DECRandom, DECUtil;
 
 type
   TFormMain = class(TForm)
-    Button1: TButton;
-    Edit1: TEdit;
+    ButtonEncrypt: TButton;
+    EditEncrypt: TEdit;
     ProgressBar1: TProgressBar;
     RadioButtonMethod: TRadioButton;
     RadioButtonProcedure: TRadioButton;
     RadioButtonAnonMethod: TRadioButton;
-    procedure Button1Click(Sender: TObject);
-  public
+    CheckBoxPKCS7: TCheckBox;
+    PageControl1: TPageControl;
+    tabEncrypt: TTabSheet;
+    tabDecrypt: TTabSheet;
+    ButtonDecrypt: TButton;
+    EditDecrypt: TEdit;
+    EditKey: TLabeledEdit;
+    EditIV: TLabeledEdit;
+    ButtonCreateKeyAndIV: TButton;
+    procedure ButtonEncryptClick(Sender: TObject);
+    procedure ButtonDecryptClick(Sender: TObject);
+    procedure ButtonCreateKeyAndIVClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    fCipher: TCipher_AES;
     procedure OnProgress(Size, Pos: Int64; State: TDECProgressState);
   end;
 
@@ -46,12 +61,31 @@ var
 implementation
 
 uses
-  System.UITypes, DECCiphers, DECCipherBase;
+  System.UITypes;
 
 {$R *.dfm}
 
 resourcestring
   rFileNameEmptyFailure = 'No input file specified!';
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  fCipher := TCipher_AES.Create;
+  fCipher.Mode := cmCBCx;
+end;
+
+procedure TFormMain.FormDestroy(Sender: TObject);
+begin
+  fCipher.Free;
+end;
+
+procedure TFormMain.ButtonCreateKeyAndIVClick(Sender: TObject);
+begin
+  EditKey.Text := StringOf(TFormat_Base64.Encode(
+    RandomBytes(fCipher.Context.KeySize)));
+  EditIV.Text := StringOf(TFormat_Base64.Encode(
+    RandomBytes(fCipher.Context.BlockSize)));
+end;
 
 procedure OnProgressProc(Size, Pos: Int64; State: TDECProgressState);
 begin
@@ -64,58 +98,6 @@ begin
     FormMain.ProgressBar1.Position := Pos;
 end;
 
-procedure TFormMain.Button1Click(Sender: TObject);
-var
-  Cipher     : TCipher_AES;
-  TargetFile : string;
-begin
-  if Edit1.Text = '' then
-  begin
-    MessageDlg(rFileNameEmptyFailure, mtError, [mbOK], -1);
-    exit;
-  end;
-
-  Cipher := TCipher_AES.Create;
-
-  try
-    try
-      // Init encryption
-      Cipher.Init(RawByteString('Passwort1234567890'), RawByteString(#1#2#3#4#5#6#7#99), 0);
-      Cipher.Mode := cmCBCx;
-
-      // replace file extension of input file
-      TargetFile := Edit1.Text;
-      Delete(TargetFile, pos('.', TargetFile), length(TargetFile));
-      TargetFile := TargetFile + '.enc';
-
-      // depending on selected radio button demo a different progress event technique
-      if RadioButtonMethod.Checked then
-        Cipher.EncodeFile(Edit1.Text, TargetFile, OnProgress)
-      else
-        if RadioButtonProcedure.Checked then
-          Cipher.EncodeFile(Edit1.Text, TargetFile, OnProgressProc)
-        else
-          if RadioButtonAnonMethod.Checked then
-            Cipher.EncodeFile(Edit1.Text, TargetFile,
-                              procedure(Size, Pos: Int64; State: TDECProgressState)
-                              begin
-                                ProgressBar1.Min := 0;
-                                ProgressBar1.Max := Size;
-
-                                if (State = Finished) then
-                                  ProgressBar1.Position := ProgressBar1.Max
-                                else
-                                  ProgressBar1.Position := Pos;
-                              end);
-    except
-      on E: Exception do
-        MessageDlg(E.Message, mtError, [mbOK], -1);
-    end;
-  finally
-    Cipher.Free;
-  end;
-end;
-
 procedure TFormMain.OnProgress(Size, Pos: Int64; State: TDECProgressState);
 begin
   ProgressBar1.Min := 0;
@@ -125,6 +107,108 @@ begin
     ProgressBar1.Position := ProgressBar1.Max
   else
     ProgressBar1.Position := Pos;
+end;
+
+procedure TFormMain.ButtonEncryptClick(Sender: TObject);
+var
+  SourceFile, DestFile: string;
+  PaddingMode: TPaddingMode;
+begin
+  if EditEncrypt.Text = '' then
+  begin
+    MessageDlg(rFileNameEmptyFailure, mtError, [mbOK], -1);
+    exit;
+  end;
+
+  if CheckBoxPKCS7.Checked then
+    PaddingMode := pmPKCS7
+  else
+    PaddingMode := pmNone;
+
+  try
+    // Init encryption
+    fCipher.Init(
+      RawStringToBytes(TFormat_Base64.Decode(RawByteString(EditKey.Text))),
+      RawStringToBytes(TFormat_Base64.Decode(RawByteString(EditIV.Text))),
+      0,
+      PaddingMode);
+
+    // replace file extension of input file
+    SourceFile := EditEncrypt.Text;
+    DestFile := ChangeFileExt(SourceFile, '.enc');
+
+    // depending on selected radio button demo a different progress event technique
+    if RadioButtonMethod.Checked then
+      fCipher.EncodeFile(SourceFile, DestFile, OnProgress)
+    else if RadioButtonProcedure.Checked then
+      fCipher.EncodeFile(SourceFile, DestFile, OnProgressProc)
+    else if RadioButtonAnonMethod.Checked then
+      fCipher.EncodeFile(SourceFile, DestFile,
+                         procedure(Size, Pos: Int64; State: TDECProgressState)
+                         begin
+                           ProgressBar1.Min := 0;
+                           ProgressBar1.Max := Size;
+
+                           if (State = Finished) then
+                             ProgressBar1.Position := ProgressBar1.Max
+                           else
+                             ProgressBar1.Position := Pos;
+                         end);
+  except
+    on E: Exception do
+      MessageDlg(E.Message, mtError, [mbOK], -1);
+  end;
+end;
+
+procedure TFormMain.ButtonDecryptClick(Sender: TObject);
+var
+  SourceFile, DestFile: string;
+  PaddingMode: TPaddingMode;
+begin
+  if EditDecrypt.Text = '' then
+  begin
+    MessageDlg(rFileNameEmptyFailure, mtError, [mbOK], -1);
+    exit;
+  end;
+
+  if CheckBoxPKCS7.Checked then
+    PaddingMode := pmPKCS7
+  else
+    PaddingMode := pmNone;
+
+  try
+    // Init encryption
+    fCipher.Init(
+      RawStringToBytes(TFormat_Base64.Decode(RawByteString(EditKey.Text))),
+      RawStringToBytes(TFormat_Base64.Decode(RawByteString(EditIV.Text))),
+      0,
+      PaddingMode);
+
+    // replace file extension of input file
+    SourceFile := EditDecrypt.Text;
+    DestFile := ChangeFileExt(SourceFile, '.dec.txt');
+
+    // depending on selected radio button demo a different progress event technique
+    if RadioButtonMethod.Checked then
+      fCipher.DecodeFile(SourceFile, DestFile, OnProgress)
+    else if RadioButtonProcedure.Checked then
+      fCipher.DecodeFile(SourceFile, DestFile, OnProgressProc)
+    else if RadioButtonAnonMethod.Checked then
+      fCipher.DecodeFile(SourceFile, DestFile,
+                         procedure(Size, Pos: Int64; State: TDECProgressState)
+                         begin
+                           ProgressBar1.Min := 0;
+                           ProgressBar1.Max := Size;
+
+                           if (State = Finished) then
+                             ProgressBar1.Position := ProgressBar1.Max
+                           else
+                             ProgressBar1.Position := Pos;
+                         end);
+  except
+    on E: Exception do
+      MessageDlg(E.Message, mtError, [mbOK], -1);
+  end;
 end;
 
 end.
